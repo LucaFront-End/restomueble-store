f13e67f42486cfc4cef6f547ebd947c7aa5b299a
LucaFront-End <LucaFront-End@users.noreply.github.com>
2026-03-20 20:24:39 -0300

    feat: add delivery conditions and warranty section to product page

--- src/app/producto/[slug]/ProductPageClient.tsx
+++ src/app/producto/[slug]/ProductPageClient.tsx
@@ -6,6 +6,7 @@ import { ProductInfo } from "@/components/catalogue/ProductInfo";
 import AddToCart from "./AddToCart";
 import ColorSelector from "./ColorSelector";
 import ShippingCalculator from "@/components/ShippingCalculator";
+import DeliveryConditions from "@/components/DeliveryConditions";
 import type { ColorCombination } from "@/lib/wixCmsColores";
 
 interface ProductPageClientProps {
@@ -55,6 +56,7 @@ export default function ProductPageClient({ product, colorData = [] }: ProductPa
                     />
 
                     <ShippingCalculator />
+                    <DeliveryConditions />
                 </ProductInfo>
             </div>
         </div>
